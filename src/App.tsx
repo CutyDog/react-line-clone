d66d25ec959b31a4fbb996ea723c5e3adf7d090a
CutyDog <silver.soul.818@gmail.com>
2022-10-23 12:44:05 +0900

    base src

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import Line from './components/Line';
+import SignIn from './components/SignIn';
+import { auth } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 function App() {
-  return <div className="App"></div>;
+  const [user] = useAuthState(auth);
+  return <div className="App">{user ? <Line /> : <SignIn />}</div>;
 }
 
 export default App;
